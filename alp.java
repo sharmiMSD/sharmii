ed4507b73efee65190d3b826371a29712f8719e1
sharmiMSD <30532307+sharmiMSD@users.noreply.github.com>
2018-06-22 14:37:35 +0530

    Create alp.java

---
+++ alp.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class alp
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+char c=sc.next();
+System.out.print("Enter the character");
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E||ch=='I'||ch=='O'||ch=='U')
+{
+System.out.println("Entered character is vowel");
+}
+else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+System.out.print("Entered character is consonant");
+else
+{
+System.out.print("not an alphabet"
