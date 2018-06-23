93932de1c5c4f8995b942a33a82e1cc1af02ca0b
sharmiMSD <30532307+sharmiMSD@users.noreply.github.com>
2018-06-23 09:36:58 +0530

    Create vow.java

---
+++ vow.java
@@ -0,0 +1,23 @@
+import java.io.*:
+import java.util.*;
+class vow
+{
+public static void main(String arg[])
+{
+Sacnner sc=new Scanner(System.in);
+char c=sc.next();
+System.out.print('Enter the character");
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+System.out.print("its vowel");
+}
+else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+{
+System.out.print("its consonant");
+}
+else
+{
+System.out.print("not an alphabet");
+}
+}
+}
